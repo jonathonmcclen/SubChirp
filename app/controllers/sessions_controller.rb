3c934aa357ce17871cc52cee9638a7e37d9a91b9
--replace-all <jonathonsmcclendon@gmail.com>
2022-01-12 16:45:06 -0600

    session creation refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,33 +14,40 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-        user = nil
-
         if params[:user][:username].include?("@")
-            user = User.find_by(email: params[:user][:username]).try(:authenticate, params[:user][:password])
-            byebug
+            user = User.find_by(email: params[:user][:username])
+            if user.try(:authenticate, params[:user][:password])
+                session[:id] = user.id
+                session[:username] = user.username
+                session[:status] = "created"
+                session[:logged_in] = true
+                redirect_to user_path(user)
+            else 
+                render_fail
+            end 
         else 
-            user = User.find_by(username: params[:user][:username]).try(:authenticate, params[:user][:password])
-            byebug
-        end 
-        
+            user = User.find_by(username: params[:user][:username])
+            if user.try(:authenticate, params[:user][:password])
+                session[:id] = user.id
+                session[:username] = user.username
+                session[:status] = "created"
+                session[:logged_in] = true
+                redirect_to user_path(user)
+            else 
+                render_fail
+            end 
+        end
 
-        if user
-            session[:user_id] = user.id
-            session[:username] = user.username
-            session[:status] = "created"
-            session[:logged_in] = true
-
-            redirect_to user_path(user)
-        else
+        def render_fail
             params[:message] = "So Sorry but we cant locate that username/email and password combination"
             redirect_to '/'
         end 
+
     end 
 
     def clear
         session.clear
-        render :'/application/login'
+        redirect_to '/'
     end 
     
 end 
