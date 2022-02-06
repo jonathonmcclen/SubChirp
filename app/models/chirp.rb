f902f6801cfea7f721263ed8a7794eba369fcd75
--replace-all <jonathonsmcclendon@gmail.com>
2022-02-06 13:18:40 -0600

    Chirp Validations

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -1,5 +1,17 @@
 class Chirp < ActiveRecord::Base
     belongs_to :user
+    belongs_to :tag
 
-    scope :recent, -> {  }
+    validates :user_id, presence: true
+    validates :content, presence: true
+    validates :sub_tag, presence: true
+
+    def recent 
+        self.last(5)
+    end 
+
+    scope :setto_private, -> { where(private: true) }
+    scope :setto_public, -> { where(private: false) }
+
+    #@private_chirps = @user.chirps.setto_private
 end 
