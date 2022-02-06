a967e9a12a2f48b3240b06385eaf5959bbb75aa0
--replace-all <jonathonsmcclendon@gmail.com>
2022-02-06 13:18:25 -0600

    Login Fail Messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,10 +12,15 @@ class SessionsController < ApplicationController
             
         end 
     end 
+    def render_fail
+        params[:message] = "So Sorry but we cant locate that username/email and password combination"
+        redirect_to '/'
+    end 
 
     def create 
         if params[:user][:username].include?("@")
             user = User.find_by(email: params[:user][:username])
+            
             if user.try(:authenticate, params[:user][:password])
                 session[:id] = user.id
                 session[:username] = user.username
@@ -23,7 +28,7 @@ class SessionsController < ApplicationController
                 session[:logged_in] = true
                 redirect_to user_path(user)
             else 
-                render_fail
+                render_fail()
             end 
         else 
             user = User.find_by(username: params[:user][:username])
@@ -34,14 +39,11 @@ class SessionsController < ApplicationController
                 session[:logged_in] = true
                 redirect_to user_path(user)
             else 
-                render_fail
+                render_fail()
             end 
         end
 
-        def render_fail
-            params[:message] = "So Sorry but we cant locate that username/email and password combination"
-            redirect_to '/'
-        end 
+        
 
     end 
 
