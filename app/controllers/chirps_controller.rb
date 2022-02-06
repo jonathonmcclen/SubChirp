aa2a257ae4e307982645af0cabe04a1d2b69acfe
--replace-all <jonathonsmcclendon@gmail.com>
2022-02-06 13:18:09 -0600

    made private Chirps PRIVATE

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -2,11 +2,19 @@ class ChirpsController < ApplicationController
 
     def show
         @chirp = Chirp.find(params[:id])
-        render chirp_path(@chirp)
+
+        if session[:id] == @chirp.user_id
+            render :show
+        elsif @chirp.private == true
+            render :'private'
+        end
+
     end
 
     def index
-        
+        @user = User.find(params[:user_id])
+        @chips = @user.chirps.setto_public
+        render :all_chirps
     end
     
     def new
@@ -20,9 +28,14 @@ class ChirpsController < ApplicationController
     end
 
     def create
-
+        @chirp = Chirp.new
+        @chirp.sub_tag = params[:chirp][:sub_tag]
+        @chirp.content = params[:chirp][:content]
+        params[:chirp][:private].to_i == 1 ? @chirp.private = true : @chirp.private = false
+        @chirp.user = User.find(session[:id])
+        @chirp.tag_id = params[:chirp][:tag_id]
+        @chirp.save
+        redirect_to "/chirps/#{@chirp[:id]}"
     end
-
-
 
 end 
