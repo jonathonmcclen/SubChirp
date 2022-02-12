cf5f2b34d2e2702f06e577175bd006bb220a122c
--replace-all <jonathonsmcclendon@gmail.com>
2022-02-12 12:36:04 -0600

    Create layouts_helper.rb

---
+++ app/helpers/layouts_helper.rb
@@ -0,0 +1,14 @@
+module LayoutsHelper
+
+    def show_logged_in_nav
+
+        logged_user = User.find(session[:id])
+
+        if session[:id]
+            `<div id="top-bar" style="width: 100%; background-color: grey; color: white; height: 20px; margin: 0; padding: 20px 50px;">
+              <a href="/users/<%=session[:id]%>/chirps/new"> <button>Create Chirp</button></a> <button>notifications</button>
+            </div>`
+        end
+    end 
+
+end
