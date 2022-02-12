004f712349d44205749d671018d734d7eb0c7005
--replace-all <jonathonsmcclendon@gmail.com>
2022-02-12 12:35:18 -0600

    helpermethod for rendering bar that is only visable when a user is logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    def logged_in_nav
+
+        @logged_user = User.find(session[:id])
+
+        if session[:id]
+            `<div id="top-bar" style="width: 100%; background-color: grey; color: white; height: 20px; margin: 0; padding: 20px 50px;">
+              <a href="/users/<%=session[:id]%>/chirps/new"> <button>Create Chirp</button></a> <button>notifications</button>
+            </div>`
+        end
+    end 
 end
